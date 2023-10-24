4b35b345e7b9e0628fdea16dff3427290990f531
abyal <alex.aby555@gmail.com>
2023-10-24 13:14:18 +0100

    image creation with the auto aws imaage

--- terraform_v1.tf
+++ terraform_v1.tf
@@ -15,6 +15,12 @@ data "aws_ami" "centos" {
   owners = ["973714476881"]
   name_regex = "Centos-8-DevOps-Practice"
 }
-output "ami" {
-  value = "data.aws_ami.centos.image.id"
+
+resource "aws_instance" "frontend" {
+  ami           = "data.aws_ami.centos.image.id"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "frontend"
+  }
 }
