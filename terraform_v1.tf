4a12764bac58fecb6330505e4abef691f2ca0ff6
abyal <alex.aby555@gmail.com>
2023-10-24 12:54:20 +0100

    image id detection

--- terraform_v1.tf
+++ terraform_v1.tf
@@ -1,12 +1,20 @@
-resource "aws_instance" "frontend" {
-  ami           = "ami-03265a0778a880afb"
-  instance_type = "t3.micro"
+#resource "aws_instance" "frontend" {
+#  ami           = "ami-03265a0778a880afb"
+#  instance_type = "t3.micro"
+#
+#  tags = {
+#    Name = "frontend"
+#  }
+#}
 
-  tags = {
-    Name = "frontend"
-  }
+#output "frontend" {
+#  value = aws_instance.frontend.public_ip
+#}
+
+data "aws_ami" "centos" {
+  owners = ["973714476881"]
+  name_regex = "Centos-8-DevOps-Practice"
 }
-
-output "frontend" {
-  value = aws_instance.frontend.public_ip
+output "ami" {
+  value = "data.aws_ami.centos.image.id"
 }
